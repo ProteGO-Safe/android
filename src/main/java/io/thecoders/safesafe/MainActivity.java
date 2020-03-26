680ea51bc78fe9862f6b267f1d3a7083fca938a1
Michał Sikora <me@mehow.io>
2020-03-26 20:00:52 +0100

    Initial commit.

---
+++ src/main/java/io/thecoders/safesafe/MainActivity.java
@@ -0,0 +1,11 @@
+package io.thecoders.safesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+  @Override protected void onCreate(@Nullable Bundle inState) {
+    super.onCreate(inState);
+  }
+}
