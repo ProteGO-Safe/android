69de24c36587c9e4d24fa1e2ec61c2a076b18759
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2020-03-31 22:55:22 +0200

    Zaktualizowany identyfikator producenta

--- app/src/main/kotlin/pl/gov/mc/protego/bluetooth/Constants.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/bluetooth/Constants.kt
@@ -1,6 +1,6 @@
 package pl.gov.mc.protego.bluetooth
 
-const val ProteGoManufacturerId = 0xFFFF
+const val ProteGoManufacturerId = 0x08AF
 const val ProteGoManufacturerDataVersion = 0x00
 //const val ProteGoServiceUUIDString = "89a60000-4f57-4c1b-9042-7ed87d723b4e"
 const val ProteGoServiceUUIDString = "0000CCCC-0000-1000-8000-00805F9B34FB"
