87c4451d817ebc689e748ebb343cba2afdda63eb
Michał Sikora <me@mehow.io>
2020-03-31 17:40:01 +0200

    Load SafeSafe app in WebView.

--- src/main/java/io/thecoders/safesafe/MainActivity.java
+++ src/main/java/io/thecoders/safesafe/MainActivity.java
@@ -2,10 +2,25 @@ package io.thecoders.safesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+  private WebView webView;
+
   @Override protected void onCreate(@Nullable Bundle inState) {
     super.onCreate(inState);
+    setContentView(R.layout.content);
+    webView = findViewById(R.id.webContent);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadUrl("https://safesafe.thecoders.io/");
+  }
+
+  @Override public void onBackPressed() {
+    if (webView.canGoBack()) {
+      webView.goBack();
+    } else {
+      super.onBackPressed();
+    }
   }
 }
