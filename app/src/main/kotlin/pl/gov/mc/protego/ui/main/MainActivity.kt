da933eaa78702bfaf6b14f2afdec373972146aeb
Pawel Byszewski <pawel.byszewski@polidea.com>
2020-04-02 20:46:13 +0200

    Usunięcie testowego kodu

--- app/src/main/kotlin/pl/gov/mc/protego/ui/main/MainActivity.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/ui/main/MainActivity.kt
@@ -9,25 +9,11 @@ import pl.gov.mc.protego.R
 import pl.gov.mc.protego.information.Session
 import pl.gov.mc.protego.ui.base.BaseActivity
 import pl.gov.mc.protego.ui.registration.RegistrationActivity
-import io.realm.RealmConfiguration
-import android.widget.Toast
-import io.realm.Realm
-
-import io.realm.RealmObject
-import pl.gov.mc.protego.realm.RealmEncryption
-import pl.gov.mc.protego.realm.RealmInitializer
-
-import timber.log.Timber
-
-
-
-
 
 class MainActivity : BaseActivity() {
 
     private val viewModel: MainActivityViewModel by viewModel()
     private val session: Session by inject()
-    private val reaInitializer: RealmInitializer by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,42 +23,10 @@ class MainActivity : BaseActivity() {
             startActivity(Intent(this, RegistrationActivity::class.java))
             finish()
         }
-        reaInitializer.setup(this)
-
-        val realm = getRealm()!!
-//        realm.beginTransaction()
-//        val todoItem = realm.createObject(TodoItem::class.java)
-//        todoItem.name ="Item "
-//        realm.commitTransaction()
-
-        realm.beginTransaction()
-        val todos = realm.where(TodoItem::class.java).count()
-        Timber.d("Quantity: ${todos}")
-//        todoItem.name ="Item "
-        realm.commitTransaction()
-
-
     }
 
     override fun onResume() {
         super.onResume()
-//        viewModel.onResume()
+        viewModel.onResume()
     }
-
-    private fun getRealm(): Realm? {
-        try {
-            return Realm.getDefaultInstance()
-        } catch (e: IllegalArgumentException) {
-            Timber.e(e, "Cannot open realm")
-            Toast.makeText(this, "Please unlock Realm first.", Toast.LENGTH_SHORT).show()
-            return null
-        }
-
-    }
-
-}
-
-open class TodoItem : RealmObject() {
-    var name: String? = null
-    var isDone: Boolean = false
 }
