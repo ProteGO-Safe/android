dc0aba16d917f4a78752f1d6b1a0c37c94cc7977
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2020-04-04 15:57:58 +0200

    Dodanie walidacji długości kodu SMS

--- app/src/main/kotlin/pl/gov/mc/protego/ui/registration/RegistrationConfirmationViewModel.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/ui/registration/RegistrationConfirmationViewModel.kt
@@ -1,5 +1,6 @@
 package pl.gov.mc.protego.ui.registration
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -16,8 +17,17 @@ class RegistrationConfirmationViewModel(
 
     private var disposables = CompositeDisposable()
 
-    val confirmationError = MutableLiveData<String>()
-    val confirmationSuccess = MutableLiveData<Boolean>()
+    private val _confirmationError = MutableLiveData<String>()
+    val confirmationError: LiveData<String>
+        get() = _confirmationError
+
+    private val _confirmationSuccess = MutableLiveData<Boolean>()
+    val confirmationSuccess: LiveData<Boolean>
+        get() = _confirmationSuccess
+
+    private val _confirmationEnabled = MutableLiveData<Boolean>().apply { value = false }
+    val confirmationEnabled: LiveData<Boolean>
+        get() = _confirmationEnabled
 
     fun confirm(code: String) {
         protegoServer
@@ -27,13 +37,17 @@ class RegistrationConfirmationViewModel(
             .subscribeBy(
                 onError = {
                     Timber.e(it, "Confirmation error: ${it.message}")
-                    confirmationError.value = it.message
+                    _confirmationError.value = it.message
                 },
                 onSuccess = {
                     Timber.d("Confirmed")
-                    confirmationSuccess.value = true
+                    _confirmationSuccess.value = true
                 }
             )
             .addTo(disposables)
+    }
+
+    fun onCodeChanged(code: String) {
+        _confirmationEnabled.value = code.length == 6
     }
 }
