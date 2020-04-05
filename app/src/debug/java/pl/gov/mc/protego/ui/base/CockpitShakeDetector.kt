3e5d5e1c1e064842ead577e509ebfad34ef82342
Mikolaj Kojdecki <mikolaj.kojdecki@polidea.com>
2020-04-05 18:28:57 +0200

    Przeniesienie przycisku wylogowania do menu debugowego

--- app/src/debug/java/pl/gov/mc/protego/ui/base/CockpitShakeDetector.kt
+++ app/src/debug/java/pl/gov/mc/protego/ui/base/CockpitShakeDetector.kt
@@ -6,7 +6,7 @@ import com.squareup.seismic.ShakeDetector
 class CockpitShakeDetector {
     private var onShake: () -> Unit = {}
 
-    private val shakeDetector = ShakeDetector { onShake() }
+    private val shakeDetector = ShakeDetector { onShake() }.apply { setSensitivity(10) }
 
     fun startDetection(sensorManager: SensorManager) {
         shakeDetector.start(sensorManager)
