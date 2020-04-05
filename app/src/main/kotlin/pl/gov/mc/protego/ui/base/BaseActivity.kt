999c6dc9ac95052979f13b2b2432b332e80bc73c
Pawel Byszewski <pawel.byszewski@polidea.com>
2020-04-05 19:01:51 +0200

    poprawki po rebase

--- app/src/main/kotlin/pl/gov/mc/protego/ui/base/BaseActivity.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/ui/base/BaseActivity.kt
@@ -1,23 +1,10 @@
 package pl.gov.mc.protego.ui.base
 
-import android.content.Context
-import android.hardware.SensorManager
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.DialogFragment
-import org.koin.android.ext.android.inject
-import pl.gov.mc.protego.R
-import pl.gov.mc.protego.ui.dialog.NoInternetConnectionDialog
 import kotlinx.android.synthetic.main.toolbar.*
+import pl.gov.mc.protego.ui.dialog.NoInternetConnectionDialog
 
 
-abstract class BaseActivity : AppCompatActivity() {
-    private val shakeDetector: CockpitShakeDetector by inject()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 abstract class BaseActivity : BaseVariantActivity() {
 
     override fun setContentView(layoutResID: Int) {
