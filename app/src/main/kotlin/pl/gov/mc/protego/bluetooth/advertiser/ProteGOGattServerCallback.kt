972a03460493493881648850590d689fbb5b9ceb
Tomasz Heimowski <tomek.heimowski@gmail.com>
2020-04-09 21:39:19 +0200

    Revert ProteGoGattServerCallback.kt

--- app/src/main/kotlin/pl/gov/mc/protego/bluetooth/advertiser/ProteGOGattServerCallback.kt
+++
@@ -1,6 +0,0 @@
-package pl.gov.mc.protego.bluetooth.advertiser
-
-interface ProteGOGattServerCallback {
-    fun gattServerStarted(gattServer: ProteGOGattServer)
-    fun gattServerFailed(gattServer: ProteGOGattServer, status: Int)
-}
