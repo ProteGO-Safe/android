40e162a83318b33d0beb1bcbb9f1d4c592027180
Mikołaj Kojdecki <mikolaj.kojdecki@polidea.com>
2020-04-09 15:40:14 +0200

    Przeniesienie przycisku wylogowania do menu debugowego (#86)
    * Przeniesienie przycisku wylogowania do menu debugowego
    
    * Usunięcie źle skonfigurowanego parametru
    
    * Przywrócenie domyślnej czułości wykrywania potrząśnięć

--- app/src/main/kotlin/pl/gov/mc/protego/ui/main/fragments/dashboard/DashboardMainFragment.kt
+++ app/src/main/kotlin/pl/gov/mc/protego/ui/main/fragments/dashboard/DashboardMainFragment.kt
@@ -1,22 +1,16 @@
 package pl.gov.mc.protego.ui.main.fragments.dashboard
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_dashboard.*
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pl.gov.mc.protego.R
-import pl.gov.mc.protego.information.Session
 import pl.gov.mc.protego.ui.base.BaseFragment
-import pl.gov.mc.protego.ui.registration.onboarding.OnboardingActivity
 
 class DashboardMainFragment : BaseFragment() {
 
     override val viewModel: DashboardMainViewModel by viewModel()
-    private val session: Session by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,18 +18,6 @@ class DashboardMainFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        logout_button.setOnClickListener {
-            session.logout()
-            startActivity(Intent(requireContext(), OnboardingActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            })
-            activity?.finish()
-        }
     }
 }
 
