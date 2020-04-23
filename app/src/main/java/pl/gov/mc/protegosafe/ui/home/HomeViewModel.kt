d382ae7bd8c1fc339ec51ff3c42e9522276a6ed2
wojciech <wojciech.nasilowski@sigmaconnectivity.se>
2020-04-23 10:41:39 +0200

    fixup

--- app/src/main/java/pl/gov/mc/protegosafe/ui/home/HomeViewModel.kt
+++ app/src/main/java/pl/gov/mc/protegosafe/ui/home/HomeViewModel.kt
@@ -2,29 +2,21 @@ package pl.gov.mc.protegosafe.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import org.json.JSONObject
 import pl.gov.mc.protegosafe.domain.model.IncomingBridgeDataItem
 import pl.gov.mc.protegosafe.domain.model.IncomingBridgeDataType
 import pl.gov.mc.protegosafe.domain.model.OutgoingBridgeDataType
-import pl.gov.mc.protegosafe.domain.usecase.GetInternetConnectionStatusUseCase
-import pl.gov.mc.protegosafe.domain.usecase.GetServicesStatusUseCase
-import pl.gov.mc.protegosafe.domain.usecase.OnGetBridgeDataUseCase
-import pl.gov.mc.protegosafe.domain.usecase.OnSetBridgeDataUseCase
-import pl.gov.mc.protegosafe.domain.usecase.TrackServiceEnabledUseCase
+import pl.gov.mc.protegosafe.domain.usecase.*
 import pl.gov.mc.protegosafe.ui.common.BaseViewModel
 import pl.gov.mc.protegosafe.ui.common.livedata.SingleLiveEvent
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
 class HomeViewModel(
     private val onSetBridgeDataUseCase: OnSetBridgeDataUseCase,
-    private val onGetBridgeDataUseCase: OnGetBridgeDataUseCase,
     private val servicesStatusUseCase: GetServicesStatusUseCase,
-    private val trackServiceEnabledUseCase: TrackServiceEnabledUseCase
+    trackServiceEnabledUseCase: TrackServiceEnabledUseCase,
     private val onGetBridgeDataUseCase: OnGetBridgeDataUseCase,
     private val internetConnectionStatusUseCase: GetInternetConnectionStatusUseCase
 ) : BaseViewModel() {
