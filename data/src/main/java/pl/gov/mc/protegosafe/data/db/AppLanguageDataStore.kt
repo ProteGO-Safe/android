0fb3a3786cf7b37d7f75556c098b3996b79f885b
Mateusz Wlazlowski <mateusz.wlazlowski@sigma.se>
2020-08-17 08:39:08 +0200

    Fix
    Signed-off-by: Mateusz Wlazlowski <mateusz.wlazlowski@sigma.se>

--- data/src/main/java/pl/gov/mc/protegosafe/data/db/AppLanguageDataStore.kt
+++ data/src/main/java/pl/gov/mc/protegosafe/data/db/AppLanguageDataStore.kt
@@ -3,7 +3,7 @@ package pl.gov.mc.protegosafe.data.db
 import java.util.Locale
 
 private const val SHARED_PREFS_APP_LANG_ISO =
-    "data.db.TriageDataStore-app-lang-iso"
+    "data.db.AppLanguageDataStore-app-lang-iso"
 
 class AppLanguageDataStore(sharedPreferencesDelegates: SharedPreferencesDelegates) {
 
