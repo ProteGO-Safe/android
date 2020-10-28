63f862422c2e165c02f795d5282e6170bcc898b3
Pawel Kleczkowski <pkleczko@gmail.com>
2020-10-28 23:42:13 +0100

    Fix wrong access token model for subscription

--- data/src/main/java/pl/gov/mc/protegosafe/data/model/GetTokenResponseData.kt
+++ data/src/main/java/pl/gov/mc/protegosafe/data/model/GetTokenResponseData.kt
@@ -2,7 +2,7 @@ package pl.gov.mc.protegosafe.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class GetAccessTokenResponseData(
-    @SerializedName("accessToken")
+data class GetTokenResponseData(
+    @SerializedName("token")
     val token: String
 )
