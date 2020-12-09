904bcae101fc845f333c1f94f3c6c0c9b1419d6e
Mateusz Wlazlowski <mateusz.wlazlowski@sigma.se>
2020-12-09 14:31:13 +0100

    [PSAFE-3229]Handle new FCM data format
    Signed-off-by: Mateusz Wlazlowski <mateusz.wlazlowski@sigma.se>

--- data/src/main/java/pl/gov/mc/protegosafe/data/model/PushNotificationContentData.kt
+++ data/src/main/java/pl/gov/mc/protegosafe/data/model/PushNotificationContentData.kt
@@ -3,12 +3,6 @@ package pl.gov.mc.protegosafe.data.model
 import com.google.gson.annotations.SerializedName
 
 data class PushNotificationContentData(
-    @SerializedName("title")
-    val title: String,
-    @SerializedName("content")
-    val content: String,
-    @SerializedName("status")
-    val status: String,
     @SerializedName("localizedNotifications")
     val localizedNotifications: List<PushNotificationLocalizedContentData>
 )
